e5047794122068ab3b7ede301439a55fe68ffbf7
Aaron McPeek <arn1313@comcast.net>
2017-07-24 11:12:29 -0700

    done with todos

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -134,10 +134,13 @@ articleView.initIndexPage = function() {
 };
 
 articleView.initAdminPage = function() {
-  // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
+  // DONE: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
   // we are then calling "template" on line 117.
-
+  articleView.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#stats-template').text());
+    return template(this);
+  }
   // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
   // appending to the DOM.
   // The callback is not required to return anything.
